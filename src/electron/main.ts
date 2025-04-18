494b52a6274471a68eaa02dd006ab1e642076c38
Adiahn <adnanmukhtar2321@gmail.com>
2025-04-18 13:54:46 +0100

    Set Typescript

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,7 +1,7 @@
 import {app, BrowserWindow} from 'electron'
 import path from 'path'
 
-type test = String;
+// type test = String;
 
 app.on("ready", ()=> {
     const mainWindow = new BrowserWindow({})
